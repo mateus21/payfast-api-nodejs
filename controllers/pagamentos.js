b27870c2633fb8596c32d425071f9710fb95f0d2
Mateus Mantovani <mateus@codebit.com.br>
2018-09-11 12:49:32 -0300

    finalizado mecânica para receber request

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,5 +1,15 @@
 module.exports = function(app) {
     app.get("/pagamentos",function(req, res) {
          res.send('ok');
-     });
+    });
+
+    app.post("/pagamentos/pagamento", function(req, res) {
+        var pagamento = req.body;
+
+        pagamento.status = 'CRIADO';
+        pagamento.date = new Date;
+        console.log(pagamento);
+
+        res.send(pagamento);
+    });
  }
