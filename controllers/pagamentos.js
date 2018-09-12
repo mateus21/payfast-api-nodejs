ceab35691a58226dc26917812ed8f7127163e373
Mateus Mantovani <mateus@codebit.com.br>
2018-09-12 00:09:43 -0300

    atualizando status de um pagamento

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -3,6 +3,26 @@ module.exports = function(app) {
          res.send('ok');
     });
 
+    app.put('/pagamentos/pagamento/:id', function(req, res) {
+        var pagamento = {};
+        var id = req.params.id;
+
+        pagamento.id = id;
+        pagamento.status = 'CONFIRMADO';
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.atualiza(pagamento, function(erro) {
+            if(erro) {
+                res.status(500).send(erro);
+                return;
+            }
+
+            res.send(pagamento);
+        });
+    });
+
     app.post("/pagamentos/pagamento", function(req, res) {
         var pagamento = req.body;
         console.log('processando pagamento...');
