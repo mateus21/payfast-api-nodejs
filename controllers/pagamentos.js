0235a0edc24e37f5644fb4402c435f86c015e60b
Mateus Mantovani <mateus@codebit.com.br>
2018-09-11 23:18:47 -0300

    funcionalidades basicas da API

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -7,12 +7,31 @@ module.exports = function(app) {
         var pagamento = req.body;
         console.log('processando pagamento...');
 
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatório").notEmpty();
+        req.assert("valor", "Valor é obrigatório e deve ser um decimal").notEmpty().isFloat();
+
+        var erros = req.validationErrors();
+
+        if (erros) {
+            console.log('Erros de validacao encontrados');
+            res.status(400).send(erros);
+        }
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+
         var connection = app.persistencia.connectionFactory();
         var pagamentoDao = new app.persistencia.PagamentoDao(connection);
 
         pagamentoDao.salva(pagamento, function(exception, result) {
-            console.log('pagamento criado: ' + result);
-            res.json(pagamento);
+            if (exception) {
+                res.status(500).send(exception);
+            } else {
+                console.log('pagamento criado');
+
+                res.location('/pagamentos/pagamento/' + result.insertId);
+                res.status(201).json(pagamento);
+            }
         });
     });
  }
